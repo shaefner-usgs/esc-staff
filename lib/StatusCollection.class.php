0774762ead9af735f927836c9c3caad69431e3b4
Scott Haefner <shaefner@usgs.gov>
2017-06-21 09:34:11 -0700

    add sort method

--- lib/StatusCollection.class.php
+++ lib/StatusCollection.class.php
@@ -14,11 +14,44 @@ class StatusCollection {
   }
 
   /**
+   * Compare function
+   *
+   * @param $a {Object: Status instance}
+   * @param $b {Object: Status instance}
+   *
+   * @return {Int}
+   */
+  private function _compare ($a, $b) {
+    if ($a->begin === $b->begin) {
+      return 0;
+    }
+
+    if ($this->sortOrder === 'ASC') {
+      return ($a->begin < $b->begin) ? -1 : 1;
+    }
+    else { // DESC
+      return ($a->begin < $b->begin) ? 1 : -1;
+    }
+  }
+
+  /**
    * Add a Status instance to the Collection
    *
    * @param $Status {Object}
    */
   public function add ($Status) {
     $this->entries[] = $Status;
+  }
+
+  /**
+   * Sort by begin date
+   *
+   * @param $order {String <ASC | DESC>}
+   */
+  public function sort ($order) {
+    if ($order === 'ASC' || $order === 'DESC') {
+      $this->sortOrder = $order;
+      usort($this->entries, 'self::_compare');
+    }
   }
 }
